842e2af3ce4f1d8336f17d3b627669f275307675
davideZanin <davide.zanin@supsi.ch>
2019-05-21 23:13:37 +0200

    trying sql5

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-import asyncpg
 import asyncio
+import asyncpg
 
-conn = await asyncpg.connect(user='docker', password='docker', database='docker', host='docker')
-values = await conn.fetch('''SELECT * FROM account''')
-print(values)
-await conn.close()
+async def main():
+    # Establish a connection to an existing database named "test"
+    # as a "postgres" user.
+    conn = await asyncpg.connect(user='docker', password='docker', database='docker', host='docker')
+    # Select a row from the table.
+    res = await conn.fetch('''SELECT * FROM account''')
+    # *row* now contains
+    # asyncpg.Record(id=1, name='Bob', dob=datetime.date(1984, 3, 1))
+    print(res)
+    # Close the connection.
+    await conn.close()
+asyncio.get_event_loop().run_until_complete(main())
--- postgresqlUseful.md
+++ postgresqlUseful.md
@@ -12,3 +12,7 @@ Replace peer with md5 on local
 
 Run psql without password
 `PGPASSWORD=docker psql -d docker -U docker -w`
+
+RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+
+/usr/lib/postgresql/10/bin/postgres -D /var/lib/postgresql/10/main -c config_file=/etc/postgresql/10/main/postgresql.conf
